d544c2c6c6ca72370c639071e20b04dd87917e4b
edward01s2 <edward.01s2@gmail.com>
2020-11-11 19:33:24 -0600

    Add scripts to thnaks

--- templates/thank.php
+++ templates/thank.php
@@ -3,6 +3,15 @@
  * Template Name: Thank You
  */
 
+add_action( 'wp_head', 'es_header_scripts', 999 );
+function es_header_scripts() {
+  if ( $code = get_field( 'scripts'  ) ) {
+    echo "\n";
+    echo $code;
+    echo "\n";
+  }
+}
+
 add_filter( 'genesis_attr_entry', 'cc_join_attributes_entry' );
 function cc_join_attributes_entry( $attributes ) {
   $attributes['class'] .= ' white-content';
