fdc82d7959a445c9b6a0416b6b63a3dc6d7ea89d
Edward Shannon <edward.01s2@gmail.com>
2021-09-13 10:44:04 -0600

    Add form title

--- templates/voice.php
+++ templates/voice.php
@@ -44,7 +44,7 @@ function es_sections() {
 
   <section id="voice-letter" class="p-8 mb-24 bg-c-blue-400 md:p-12 xl:p-16">
     <div class="text-white">
-      <h3 class="text-center">Sign the letter here</h3>
+      <h3 class="text-center"><?php echo get_field('form_title') ?></h3>
       <div class="voice-form xl:max-w-[100rem] xl:mx-auto">
         <?php
           $form = get_field('form')['id'];
@@ -55,47 +55,6 @@ function es_sections() {
     </div>
   </section>
 
-  <!-- 
-  
-  <section>
-    <div class="bg-center bg-cover container-full" style="background-image: url('<?php echo $bg ?>');">
-      <div class="wrap">
-        <h1 class="py-24 mb-0 text-5xl text-center text-white md:py-40 lg:text-6xl">Our letter to<?php echo ' ' . $title ?> policy makers:</h1>
-      </div>
-    </div>
-  </section>
-
-  <?php if($state_id) : ?>
-  <section>
-    <div class="pb-16 lg:max-w-6xl lg:mx-auto lg:pb-24 lg:pt-16 xl:max-w-7xl">
-      <a class="inline-block py-8 mb-0 font-bold text-c-teal-100 lg:pb-12" href="/<?php echo $title ?>">
-        [ 
-        <svg class="inline-block w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="1" height="1" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" />
-        </svg>
-        Back ]
-      </a>
-      <?php echo $letter ?>
-    </div>
-  </section>
-  <?php endif; ?>
-
-  <section>
-    <div class="container-full bg-c-blue-50">
-      <div class="py-24 wrap md:py-32">
-        <div class="lg:max-w-5xl lg:mx-auto">
-          <h3 class="mb-12 text-center md:mb-16">Add your name</h3>
-          <div>
-            <?php
-              $form = get_field('letter form', $state_id);
-              $form = ($form) ? $form : 17; 
-              gravity_form( $form, false, false, false, null, true, -1 ); 
-            ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section> -->
 <?php
 }
 
